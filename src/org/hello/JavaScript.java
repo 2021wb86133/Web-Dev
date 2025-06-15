616ba0f93d03d9cf40dbc8d0efef4ffac2c34608
2021wb86133 <2021WB86133@wilp.bits-pilani.ac.in>
2025-06-15 21:47:35 +0530

    Update JavaScript.java

--- src/org/hello/JavaScript.java
+++ src/org/hello/JavaScript.java
@@ -3,7 +3,7 @@ import java.util.logging.Logger;
  
 public class JavaScript {
  
-    private static final Logger logger = Logger.getLogger(HelloBITS.class.getName());
+    private static final Logger logger = Logger.getLogger(JavaScript.class.getName());
  
     public static void main(String[] args) {
         logger.info("Hello BITS from SonarQube analysis!");
