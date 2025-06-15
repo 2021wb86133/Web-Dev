6c4fb904cebd796b43ff4f48aa08485d03d323a2
2021wb86133 <2021WB86133@wilp.bits-pilani.ac.in>
2025-06-15 21:40:36 +0530

    Update JavaScript.java

--- src/org/hello/JavaScript.java
+++ src/org/hello/JavaScript.java
@@ -1,6 +1,6 @@
 package org.hello;
 public class JavaScript{
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.err.println("Hello, World!");
     }
 }
